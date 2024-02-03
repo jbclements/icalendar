48e87ed2e84cff83f1a3e3838cbdbf9ba7284d8d
John Clements <clements@racket-lang.org>
2024-02-03 09:44:14 -0800

    first commit, WIP WIP

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # icalendar
+
+JUST getting started, having fun reading the RFC and thinking about parsing.
+
+
 ical read/write
---
+++ parse.rkt
@@ -0,0 +1,70 @@
+#lang racket
+
+;; following along with RFC 5545 ...
+
+(require rackunit)
+
+(define in-port (open-input-file "/tmp/basic.ics"))
+
+;; read all \r\n-terminated lines
+(define lines
+  (let loop ([lines '()])
+    (define try-match
+      (regexp-match #px#"^[^\r]*\r\n" in-port))
+    (cond [try-match
+           (loop (cons (first try-match) lines))]
+          [else
+           (reverse lines)])))
+
+;; join using "unfolding"
+(define (unfold lines)
+  (let loop ([lines lines]
+             [accum '()])
+    (define (advance) (loop (rest lines) (cons (first lines) accum)))
+    (cond [(empty? lines) (reverse accum)]
+          [(empty? (rest lines)) (advance)]
+          ;; could avoid this regexp if it turns out to be slow...
+          [(regexp-match #px#"^[ \t]" (second lines))
+           (loop (cons (bytes-append (subbytes (first lines) 0
+                                               (- (bytes-length (first lines)) 2))
+                                     (subbytes (second lines) 1))
+                       (rest (rest lines)))
+                 accum)]
+          [else (advance)])))
+
+
+
+(check-equal? (unfold '(#"abc\r\n" #"def\r\n" #"ghi\r\n"))
+              '(#"abc\r\n" #"def\r\n" #"ghi\r\n")
+              "t1")
+(check-equal? (unfold '(#"abc\r\n" #" def\r\n" #"ghi\r\n"))
+              '(#"abcdef\r\n" #"ghi\r\n"))
+(check-equal? (unfold '(#"xx\r\n" #"abc\r\n" #" def\r\n" #"ghi\r\n"))
+              '(#"xx\r\n" #"abcdef\r\n" #"ghi\r\n"))
+(check-equal? (unfold '(#"abc\r\n" #" def\r\n" #"ghi\r\n" #"xx\r\n"))
+              '(#"abcdef\r\n" #"ghi\r\n" #"xx\r\n"))
+(check-equal? (unfold '(#"yy\r\n" #"abc\r\n" #" def\r\n" #"ghi\r\n" #"x x\r\n"))
+              '(#"yy\r\n" #"abcdef\r\n" #"ghi\r\n" #"x x\r\n"))
+(check-equal? (unfold '(#"abc\r\n" #" def\r\n" #" ghi\r\n"))
+              '(#"abcdefghi\r\n" ))
+(check-equal? (unfold '(#"xx\r\n" #"abc\r\n" #" def\r\n" #" ghi\r\n"))
+              '(#"xx\r\n" #"abcdefghi\r\n" ))
+(check-equal? (unfold '(#"xx\r\n" #"abc\r\n" #" def\r\n" #" ghi\r\n" #"yy\r\n"))
+              '(#"xx\r\n" #"abcdefghi\r\n" #"yy\r\n"))
+(check-equal? (unfold '(#"abc\r\n" #" def\r\n" #" ghi\r\n" #"yy\r\n"))
+              '(#"abcdefghi\r\n" #"yy\r\n"))
+
+
+
+(count (λ (l) (regexp-match? #px#"^[ \t]" l)) lines)
+
+(define unfolded (unfold lines))
+
+(length lines)
+(length unfolded)
+
+(define (parse-line line)
+  (match (bytes->string/utf-8 line)
+    [(regexp #px"^([-[:alnum:]]*)(.*)\r\n" (list _ name rest)) (list name rest)]))
+
+(define parsed (map parse-line unfolded))
